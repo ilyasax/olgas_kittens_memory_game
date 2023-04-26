4f8f209632fde6f360d2fbd991d545c60673812c
ilyasax <33786804+ilyasax@users.noreply.github.com>
2023-04-26 23:42:48 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -117,7 +117,7 @@ const restart = (text) => {
     });
     playerLives = 7;
     playerLivesCount.textContent = playerLives;
-    setTimeout(() => window.alert(text), 100);
+    setTimeout(() => window.alert(text), 500);
 };
 
 cardGenerator();
